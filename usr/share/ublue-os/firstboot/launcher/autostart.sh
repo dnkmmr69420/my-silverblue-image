194a1f854e4281d80bb473d87f2374aab87845b2
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-15 10:25:10 -0500

    Update autostart.sh

--- usr/share/ublue-os/firstboot/launcher/autostart.sh
+++ usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Runs nix setup
+/usr/share/ublue-os/nix/nix-setup.sh
+
 # Simply launches the "yafti" GUI with the uBlue image's configuration.
 /usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
