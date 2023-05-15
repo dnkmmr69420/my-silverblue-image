08a93514ed50db605a26bc1c560ffad8953369f6
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-15 11:05:34 -0500

    Delete nix-setup.sh

--- usr/share/ublue-os/nix/nix-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sleep 1
-/usr/bin/ublue-nix-install
-sleep 1
-sudo nix profile install nixpkgs#trash-cli nixpkgs#distrobox nixpkgs#toolbox nixpkgs#htop
-sudo nix profile install nixpkgs#gnome-text-editor nixpkgs#gnome.gnome-calander nixpkgs#gnome.gnome-clocks nixpkgs#baobab nixpkgs#gnome.gnome-weather
-
