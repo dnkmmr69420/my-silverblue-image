3adeb10d770158a23b60efe4ba9308e4fb58b485
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-15 11:35:31 -0500

    Update nix-setup.sh

--- usr/share/ublue-os/nix/nix-setup.sh
+++ usr/share/ublue-os/nix/nix-setup.sh
@@ -3,4 +3,4 @@ sleep 1
 bash /usr/share/ublue-os/nix/ublue-nix-install.sh
 sleep 1
 sudo nix profile install nixpkgs#trash-cli nixpkgs#distrobox nixpkgs#toolbox nixpkgs#htop
-sudo nix profile install nixpkgs#gnome-text-editor nixpkgs#gnome.gnome-calander nixpkgs#gnome.gnome-clocks nixpkgs#baobab nixpkgs#gnome.gnome-weather
+sudo nix profile install nixpkgs#gnome-text-editor nixpkgs#gnome.gnome-clocks nixpkgs#baobab nixpkgs#gnome.gnome-weather
